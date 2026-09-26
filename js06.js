b60b098cfcd88abf4f833a31e849658e02c531ea
AdiX7pro <adhamchoriyev234@mail.ru>
2026-09-26 12:43:30 +0500

    work with objects

---
+++ js06.js
@@ -0,0 +1,12 @@
+// UNDIFINED
+
+let username;
+console.log(username); //undefined
+let user = null;
+console.log(user); //null
+if (user == null){
+    console.log("User is null or undefined");
+}
+if (typeof username == "undefined"){
+    console.log("Username is undefined");
+}
---
+++ object07.js
@@ -0,0 +1,9 @@
+let user = {
+    name: "Adham",
+    age: 25,
+    job: "Backend Developer"
+};
+
+console.log(user.name);
+console.log(user.age);
+console.log(user.job);  
