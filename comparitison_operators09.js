7778830427174e0c6b10af3108216d061830c60b
AdiX7pro <adhamchoriyev234@mail.ru>
2026-09-26 15:19:41 +0500

    until 6th lesson

---
+++ comparitison_operators09.js
@@ -0,0 +1,9 @@
+let age = 22;
+
+console.log(`Kichik=>  ${age < 18}`);
+console.log(`Katta=>  ${age > 18}`);
+console.log(`Katta yoki teng=>  ${age >= 18}`);
+console.log(`Kichik yoki teng=>  ${age <= 18}`);
+console.log(`Teng=>  ${age == 18}`);
+console.log(`Teng emas=>  ${age != 18}`);
+console.log(`Teng ham, type ham=>  ${age === 18}`);
---
+++ if_else010.js
@@ -0,0 +1,7 @@
+let age = 22;
+if (age < 18){
+    console.log("You are a minor.");
+}
+else{
+    console.log("You are an adult.");
+}
